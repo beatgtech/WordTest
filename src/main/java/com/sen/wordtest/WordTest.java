e87a55dc25ded9c1b0e5bd88043b37f90217a3b7
beatgtech <61498422+beatgtech@users.noreply.github.com>
2020-03-04 17:02:00 +0800

    Add files via upload

---
+++ src/main/java/com/sen/wordtest/WordTest.java
@@ -0,0 +1,86 @@
+package com.sen.wordtest;
+
+import java.util.*;
+
+public class WordTest {
+
+    private static final String[] DEFAULT_DICTIONARY = new String[]{"i", "like", "sam", "sung", "samsung", "mobile", "ice", "cream", "and", "man go"};
+
+    private static List<StringBuilder> getPossibleOutputs(String mappedString, int level, String[] dictionary, Set<String> output) {
+
+        List<StringBuilder> result = new ArrayList<StringBuilder>();
+
+        if(level == 0) {
+            for (int i = 0; i < dictionary.length; i++) {
+                result.add(new StringBuilder(dictionary[i]));
+            }
+            return result;
+        }
+        for (int i = 0; i < dictionary.length; i++) {
+            String[] subDic = new String[dictionary.length - 1];
+            for (int j = 0; j < dictionary.length; j++) {
+                if(j < i) {
+                    subDic[j] = dictionary[j];
+                } else if(j > i) {
+                    subDic[j - 1] = dictionary[j];
+                }
+            }
+            List<StringBuilder> tails = getPossibleOutputs(mappedString, level - 1, subDic, output);
+            for (int j = 0; j < tails.size(); j++) {
+                StringBuilder tmp = new StringBuilder(dictionary[i] + " " + tails.get(j));
+                if(mappedString.equals(tmp.toString().replaceAll(" ", ""))) {
+                    output.add(tmp.toString());
+                }
+                result.add(tmp);
+            }
+        }
+        return result;
+    }
+
+    private static String getAllPossibleOutput(String mappedString, String[] dictionary) {
+        Set<String> result = new HashSet<String>();
+        getPossibleOutputs(mappedString, dictionary.length - 1, dictionary, result);
+        StringBuilder output = new StringBuilder();
+        for(String tmp : result) {
+            output.append(tmp + "\n");
+        }
+        return output.toString();
+    }
+
+    public static String stage1Method(String mappedString) {
+        if(null == mappedString || "".equals(mappedString)) return "";
+        return getAllPossibleOutput(mappedString, filerDictionary(mappedString, DEFAULT_DICTIONARY));
+    }
+
+    public static String stage2Method(String mappedString, String[] customDictionary) {
+        if(null == mappedString || "".equals(mappedString) || null == customDictionary || customDictionary.length == 0) return "";
+        return getAllPossibleOutput(mappedString, filerDictionary(mappedString, customDictionary));
+    }
+
+    public static String stage3Method(String mappedString, String[] customDictionary) {
+        if(null == mappedString || "".equals(mappedString)) return "";
+        if(null == customDictionary || customDictionary.length == 0) {
+            return getAllPossibleOutput(mappedString, filerDictionary(mappedString, DEFAULT_DICTIONARY));
+        }
+        List<String> compositeArr = new ArrayList<String>(Arrays.asList(customDictionary));
+        for (int i = 0; i < DEFAULT_DICTIONARY.length; i++) {
+            if(!compositeArr.contains(DEFAULT_DICTIONARY[i])) {
+                compositeArr.add(DEFAULT_DICTIONARY[i]);
+            }
+        }
+
+        String[] compositeDic = compositeArr.toArray(new String[compositeArr.size()]);
+        return getAllPossibleOutput(mappedString, filerDictionary(mappedString, compositeDic));
+    }
+
+    private static String[] filerDictionary(String mappedString, String[] dictionary) {
+        List<String> availableList = new ArrayList<String>();
+        for (int i = 0; i < dictionary.length; i++) {
+            if(mappedString.contains(dictionary[i].replaceAll(" ", ""))) {
+                availableList.add(dictionary[i]);
+            }
+        }
+        String[] availableDic = availableList.toArray(new String[availableList.size()]);
+        return availableDic;
+    }
+}
---
+++ src/test/java/com/sen/wordtest/WordTestTest.java
@@ -0,0 +1,77 @@
+package com.sen.wordtest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WordTestTest {
+
+    @Test
+    public void testStage1_1() {
+        Assert.assertEquals("i like ice cream and man go\n", WordTest.stage1Method("ilikeicecreamandmango"));
+    }
+
+    @Test
+    public void testStage1_2() {
+        Assert.assertEquals("i like sam sung mobile\ni like samsung mobile\n", WordTest.stage1Method("ilikesamsungmobile"));
+    }
+
+    @Test
+    public void testStage1_3() {
+        Assert.assertEquals("", WordTest.stage1Method(""));
+    }
+
+    @Test
+    public void testStage1_4() {
+        Assert.assertEquals("", WordTest.stage1Method(null));
+    }
+
+    @Test
+    public void testStage2_1() {
+        Assert.assertEquals("", WordTest.stage2Method("asdfasd", new String[]{}));
+    }
+
+    @Test
+    public void testStage2_2() {
+        Assert.assertEquals("", WordTest.stage2Method("asdfasdf", null));
+    }
+
+    @Test
+    public void testStage2_3() {
+        Assert.assertEquals("this is a test\n", WordTest.stage2Method("thisisatest", new String[]{"is", "this", "a", "test", "good", "score"}));
+    }
+
+    @Test
+    public void testStage2_4() {
+        Assert.assertEquals("", WordTest.stage2Method("", new String[]{"is", "this", "a", "test", "good", "score"}));
+    }
+
+    @Test
+    public void testStage2_5() {
+        Assert.assertEquals("", WordTest.stage2Method(null, new String[]{"is", "this", "a", "test", "good", "score"}));
+    }
+
+    @Test
+    public void testStage3_1() {
+        Assert.assertEquals("", WordTest.stage3Method("asdfasd", new String[]{}));
+    }
+
+    @Test
+    public void testStage3_2() {
+        Assert.assertEquals("", WordTest.stage3Method("asdfasdf", null));
+    }
+
+    @Test
+    public void testStage3_3() {
+        Assert.assertEquals("this is a test\n", WordTest.stage3Method("thisisatest", new String[]{"is", "this", "a", "test", "good", "score"}));
+    }
+
+    @Test
+    public void testStage3_4() {
+        Assert.assertEquals("", WordTest.stage3Method("", new String[]{"is", "this", "a", "test", "good", "score"}));
+    }
+
+    @Test
+    public void testStage3_5() {
+        Assert.assertEquals("", WordTest.stage3Method(null, new String[]{"is", "this", "a", "test", "good", "score"}));
+    }
+}
